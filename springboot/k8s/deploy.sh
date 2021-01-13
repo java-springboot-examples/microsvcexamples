4aedd24edd2df9c2f2f676e0e7815b66c63709b2
John Su <su216@hotmail.com>
2021-01-13 13:59:21 -0500

    Set service image tag to 1.0.3

--- springboot/k8s/deploy.sh
+++ springboot/k8s/deploy.sh
@@ -4,18 +4,18 @@ source ./env.sh
 
 export NAME=customer-sb
 export IMAGE=jsu216/microsvc.sb.customer
-export IMAGE_TAG=1.0.2
+export IMAGE_TAG=1.0.3
 
 envsubst < ./template/microsvc.yml | kubectl apply -f -
 
 export NAME=account-sb
 export IMAGE=jsu216/microsvc.sb.account
-export IMAGE_TAG=1.0.2
+export IMAGE_TAG=1.0.3
 
 envsubst < ./template/microsvc.yml | kubectl apply -f -
 
 export NAME=transaction-sb
 export IMAGE=jsu216/microsvc.sb.transaction
-export IMAGE_TAG=1.0.2
+export IMAGE_TAG=1.0.3
 
 envsubst < ./template/microsvc.yml | kubectl apply -f -
